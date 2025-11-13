ef3577a034d48cce6bb49e98f671ccaf111694d8
sergi-e <ishserge16@gmail.com>
2025-11-13 20:12:51 +0000

    Party event created

---
+++ PartyEvent.java
@@ -0,0 +1,8 @@
+public class PartyEvent {
+    private String name;
+    private String location;
+    private String date;
+    private String time;
+    private double duration;
+    private String theme;
+}
