b004014d848b9a72ef54f8c8031888abec235fda
sergi-e <ishserge16@gmail.com>
2025-11-13 20:15:42 +0000

    TalkEvent Class created

---
+++ TalkEvent.java
@@ -0,0 +1,9 @@
+public class TalkEvent {
+   private String name;
+    private String location;
+    private String date;
+    private String time;
+    private double duration;
+    private String speakerName;
+    private String eventAbstract; 
+}
