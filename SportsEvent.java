ad76aba09d8e3f55226f7493255284cb75e7ca3b
sergi-e <ishserge16@gmail.com>
2025-11-13 20:14:12 +0000

    Sports Event created

---
+++ SportsEvent.java
@@ -0,0 +1,11 @@
+public class SportsEvent implements Event, SportsHandler {
+    private String name;
+    private String location;
+    private String date;
+    private String time;
+    private double duration;
+    private String team1;
+    private String team2;
+    private String sportType;
+    
+}
