5a9cd7ebb39ef12dc0dd3aa06910f9b3f23178f3
sergi-e <ishserge16@gmail.com>
2025-11-13 20:12:26 +0000

    Event interface created

---
+++ Event.java
@@ -0,0 +1,9 @@
+//Event.java
+public interface Event{
+    // Getters for common event attributes
+    String getName();
+    String getLocation();
+    String getDate();
+    String getTime();
+    double getDuration();
+  }
