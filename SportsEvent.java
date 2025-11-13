582b8486c7462329f2596284adb1a91f814c4b57
sergi-e <ishserge16@gmail.com>
2025-11-13 20:27:08 +0000

    Getters added

--- SportsEvent.java
+++ SportsEvent.java
@@ -9,8 +9,8 @@ public class SportsEvent implements Event, SportsHandler {
     private String team2;
     private String sportType;
 
+    //setters
     
-
     //getters
     public String getName() {
         return name;
@@ -35,6 +35,42 @@ public class SportsEvent implements Event, SportsHandler {
     }
     public String getSportType() {
         return sportType;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public void setTeam1(String team1) {
+        this.team1 = team1;
+    }
+
+    public void setTeam2(String team2) {
+        this.team2 = team2;
+    }
+
+    public void setSportType(String sportType) {
+        this.sportType = sportType;
+    }
+    @override
+    public String toString(){
+        
     }
 }
 
