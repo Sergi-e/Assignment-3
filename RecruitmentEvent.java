612bb9d063dbf32b056ad24ba92b4b5e4f369ad7
sergi-e <ishserge16@gmail.com>
2025-11-13 20:13:49 +0000

    Recrutment class created

---
+++ RecruitmentEvent.java
@@ -0,0 +1,9 @@
+public class RecruitmentEvent {
+    private String name;
+    private String location;
+    private String date;
+    private String time;
+    private double duration;
+    private String company;
+    private String position;
+}
